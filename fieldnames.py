6575ec53d473ec87e63a3ceec273c69561171f54
Xenobita <togundipe@live.com>
2022-02-19 16:21:43 +0000

    moved fieldnames from menus to own file

---
+++ fieldnames.py
@@ -0,0 +1,18 @@
+product_fieldnames = [
+    'Name',
+    'Price'
+    ]
+
+courier_fieldnames = [
+    'Name',
+    'Phone Number'
+    ]
+
+order_fieldnames = [
+    'Customer Name',
+    'Address',
+    'Phone Number',
+    'Products',
+    'Courier',
+    'Order Status'
+    ]
